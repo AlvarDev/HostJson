d206245cff6d21a052cede0f892c18cd71387a67
AlvarDev <deivid07.21@gmail.com>
2016-04-09 10:57:23 -0300

    json places

--- places.js
+++ places.js
@@ -1,1 +1,54 @@
-places.js
+{
+  "success": true,
+  "places": [
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_botanic_park_afternoon.jpg",
+      "name": "Jardim Botânico de Curitiba",
+      "address": "Av. Professor Lothario Meissner x Rua Engenheiro Ostoja Roguski",
+      "lat": -25.4430871,
+      "lng": -49.2403687
+    },
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_opera_arame.jpg",
+      "name": "Ópera de Arame",
+      "address": "Rua João Gava, s/n. - Abranches",
+      "lat": -25.385076,
+      "lng": -49.276626
+    },
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_panoramic_tower.jpg",
+      "name": "Torre Panorâmica",
+      "address": "Rua Professor Lycio Grein Castro Vellozo, 191",
+      "lat": -25.42418,
+      "lng": -49.294361
+    },
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_museum_oscar_niemeyer.jpg",
+      "name": "Museu Oscar Niemeyer",
+      "address": "R. Marechal Hermes, 999",
+      "lat": -25.410412,
+      "lng": -49.266944
+    },
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_tangua_park.jpg",
+      "name": "Parque Tanguá",
+      "address": "Rua Oswaldo Maciel, 0",
+      "lat": -25.37882,
+      "lng": -49.281578
+    },
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_bacacheri_park.jpg",
+      "name": "Parque Bacacheri",
+      "address": "Rua Paulo Nadolny.",
+      "lat": -25.391102,
+      "lng": -49.232866
+    },
+    {
+      "image": "https://raw.githubusercontent.com/AlvarDev/HostJson/master/img_barigui_park.jpg",
+      "name": "Parque Barigui",
+      "address": "Avenida Candido Hartmann",
+      "lat": -25.423012,
+      "lng": -49.308713
+    }
+  ]
+}
