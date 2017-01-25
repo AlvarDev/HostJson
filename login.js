35a206e37d5226aa9461877a82b1f30d77f2af00
Alvaro David <deivid07.21@gmail.com>
2017-01-25 13:41:50 -0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+{
+  "success": true,
+  "message": "Success Login",
+  "user": {
+    "id": "0",
+    "name": "AlvarDev",
+    "email": "alvardev@google.com",
+    "urlImg": "https://lh5.googleusercontent.com/-T-kdalde_Vo/VW4JwTU4sEI/AAAAAAAAIDw/T_LB0V7-z-A/w613-h605-no/karla.jpg"
+  }
+}
