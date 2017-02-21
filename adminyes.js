7f50289b7e3314012feeb2d4c07bf59ae728000b
Alvaro David <deivid07.21@gmail.com>
2017-02-21 19:26:47 -0300

    create admin.js
    fot test

---
+++ adminyes.js
@@ -0,0 +1,23 @@
+{
+  "admision": {
+    "COD_POS": "123456",
+    "APE_PAT": "Huanca",
+    "APE_MAT": "Mamani",
+    "NOM": "Alvaro David",
+    "COD_SED": "30",
+    "DES_SED": "Lima",
+    "SEM_APE": "20171",
+    "COD_MOD": "009",
+    "DES_MOD": "EXONERADO TRASLADO INTERNO",
+    "COD_ESP": "001",
+    "DES_ESP": "Ingeniería",
+    "PUN_FIN": "512",
+    "ORD_MER": "7",
+    "EST_POST": "1",
+    "RESULTADO": "ADMITIDO",
+    "FECHA_MAT": "23/02/17",
+    "HORA": "9:00 a.m.",
+    "LUGAR_MAT": "INGENIERIA Y ARQUITECTURA",
+    "FOTO": "https://avatars0.githubusercontent.com/u/4741904?v=3&s=460"
+  }
+}
