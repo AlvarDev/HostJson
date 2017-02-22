a8a585de2d4097bcfcb2968e61ef974aa12a9381
Alvaro David <deivid07.21@gmail.com>
2017-02-22 08:18:52 -0300

    create loginpav.js

---
+++ loginPav.js
@@ -0,0 +1,13 @@
+{
+  "success": true,
+  "user": {
+    "codigoEstudiante": "19142016201810",
+    "usuario": "V16201810",
+    "nombre": "Eymi Johana ",
+    "apePaterno": "Rubio ",
+    "apeMaterno": "Carbajal",
+    "correo": "eymirubio@hotmail.com",
+    "estado": 1,
+    "programa": "Maestria en Medicina"
+  }
+}
